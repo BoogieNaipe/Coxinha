bdb771917e35259d3eebab2ee7c0cbfc45c0690b
BoogieNaipe <142553202+BoogieNaipe@users.noreply.github.com>
2024-08-15 19:37:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const menuItems = [
-    { name: "Coxinha Tradicional", description: "Coxinha recheada com frango.", price: "R$10,00", image: "Imagens/Tradicional.png" },
-    { name: "Coxinha Clássica", description: "Coxinha recheada com frango e requeijão cremoso.", price: "R$10,50", image: "Imagens/Clássica.png" },
-    { name: "Coxinha Italiana", description: "Coxinha recheada com frango, queijo, presunto, calabresa e orégano.", price: "R$10,00", image: "Imagens/Italiana.png" },
-    { name: "Coxinha Baiana", description: "Coxinha recheada com frango, carne, calabresa, requeijão cremoso, azeitonas e orégano.", price: "R$10,50", image: "Imagens/Coxinha Baiana.png" },
-    { name: "Coxinha Sertaneja", description: "Coxinha recheada com charque desfiado, banana caramelizada e cream cheese, empanada na tapioca.", price: "R$12,00", image: "Imagens/Sertaneja.png" },
-    { name: "Coxinha Nordestina", description: "Coxinha recheada com carne do sol, creme de aipim e mussarela, empanada na tapioca.", price: "R$12,00", image: "Imagens/Nordestina.png" },
-    { name: "Coxinha Queijos", description: "Coxinha recheada com Provolone, mussarela e requeijão cremoso.", price: "R$12,00", image: "Imagens/Queijos.png" },
+    { name: "Coxinha Tradicional", description: "Coxinha recheada com frango.", price: "R$10,50", image: "Imagens/Tradicional.png" },
+    { name: "Coxinha Clássica", description: "Coxinha recheada com frango e requeijão cremoso.", price: "R$11,00", image: "Imagens/Clássica.png" },
+    { name: "Coxinha Italiana", description: "Coxinha recheada com frango, queijo, presunto, calabresa e orégano.", price: "R$10,50", image: "Imagens/Italiana.png" },
+    { name: "Coxinha Baiana", description: "Coxinha recheada com frango, carne, calabresa, requeijão cremoso, azeitonas e orégano.", price: "R$11,00", image: "Imagens/Coxinha Baiana.png" },
+    { name: "Coxinha Sertaneja", description: "Coxinha recheada com charque desfiado, banana caramelizada e cream cheese, empanada na tapioca.", price: "R$13,00", image: "Imagens/Sertaneja.png" },
+    { name: "Coxinha Nordestina", description: "Coxinha recheada com carne do sol, creme de aipim e mussarela, empanada na tapioca.", price: "R$13,00", image: "Imagens/Nordestina.png" },
+    { name: "Coxinha Queijos", description: "Coxinha recheada com Provolone, mussarela e requeijão cremoso.", price: "R$13,00", image: "Imagens/Queijos.png" },
     { name: "Coxinha de Camarão", description: "Coxinha recheada com camarões frescos salteados no azeite de oliva e queijo mussarela, empanada na tapioca.", price: "R$16,00", image: "Imagens/Camarão.png" }
 ];
 
